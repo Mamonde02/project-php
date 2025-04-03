ff89498a88777d9f55681573368b76a3a88663cd
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-03 22:34:01 +0800

    remove bootstrap css

--- frontend/views/login.php
+++ frontend/views/login.php
@@ -15,8 +15,7 @@ if (isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title>Login</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
