640a0f7beba093b3a97f555d64a94208b7f7b5d1
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-08 14:44:05 +0800

    remove printconsole unnecessary tag

--- frontend/views/watch.php
+++ frontend/views/watch.php
@@ -26,24 +26,10 @@ $synopsis = $data["data"][0]["synopsis"];
 $season = $data["data"][0]["season"];
 $year = $data["data"][0]["year"];
 
-print_r($id);
 
-// from react sample project
-// {animeList.map((anime) => (
-//     <li key={anime.mal_id}>
-//       {/* <strong>{anime.title}</strong> */}
-//       <strong>{anime.rank} </strong>
-//       <strong>Anime title: {anime.title} </strong>
-//       <strong>Score: {anime.score} </strong>
-//       <strong>Popularity: {anime.popularity} </strong>
-//     </li>
-//   ))}
-
-// $image = $data["data"][0]["images"]["jpg"]["large_image_url"];
 $image = $data["data"][0]["images"]["jpg"]["image_url"];
 
-print_r($image);
-
+// print_r($image);
 // print_r($score);
 // print_r($synopsis);
 // print_r($season);
@@ -66,25 +52,6 @@ print_r($image);
         <h1 class="text-3xl font-bold text-center mb-8">Anime List Collection</h1>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            <!-- Card 1 -->
-            <!-- for each php card base on fetch data -->
-
-            <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <!-- <img class="w-full h-48 object-cover" src="https://via.placeholder.com/300x200?text=Watch+1" alt="Watch 1"> -->
-                <img class="w-full h-48 object-cover" src=<?php echo $image ?> alt="Watch Anime 1">
-                <div class="p-4">
-                    <h2 class="text-xl font-semibold"><?php echo $title; ?></h2>
-                    <p class="text-gray-600">Type: <?php echo $type ?></p>
-                    <!-- extra data sample -->
-                    <p class="text-gray-600">Score: <?php echo $source ?></p>
-                    <p class="text-gray-600">Year: <?php echo $year ?></p>
-                    <!-- synopsis description design classes -->
-                    <!-- <p class="text-gray-400"><?php echo $synopsis ?></p> -->
-
-                    <p class="text-blue-500 font-bold mt-2">Rating: <?php echo $score ?></p>
-                    <button class="mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">View</button>
-                </div>
-            </div>
 
             <?php foreach ($animes as $anime): ?>
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
