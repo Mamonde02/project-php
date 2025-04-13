3a012923bc11e2becbd6cbba9fc6d9c1a50fc6c8
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-13 20:21:51 +0800

    modified dynamic url logout

--- frontend/logout.php
+++ frontend/logout.php
@@ -3,5 +3,9 @@ session_start();
 session_destroy();
 // header("Location: frontend/views/login.php");
 header("Location: views/login.php"); // Redirect to login page
+
+$baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/php-auth/';
+header("Location: " . $baseUrl . "frontend/views/login.php");
+
 exit();
 ?>
