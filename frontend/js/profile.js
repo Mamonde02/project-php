987cfbbcbbc77421cf520aba710d2859b4146707
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-14 20:36:23 +0800

    transfer profile js file

---
+++ frontend/js/profile.js
@@ -0,0 +1,7 @@
+
+function openUpdateForm(id, username, email) {
+    // Set values in the input fields inside the modal
+    document.getElementById("id").value = id;
+    document.getElementById("username").value = username;
+    document.getElementById("email").value = email;
+}
