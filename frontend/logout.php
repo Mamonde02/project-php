1eadce9aca71e963483e94dc0f80e1bada072377
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-14 20:37:29 +0800

    remove unnecessary

--- frontend/logout.php
+++ frontend/logout.php
@@ -2,7 +2,7 @@
 session_start();
 session_destroy();
 // header("Location: frontend/views/login.php");
-header("Location: views/login.php"); // Redirect to login page
+// header("Location: views/login.php"); // Redirect to login page
 
 $baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/php-auth/';
 header("Location: " . $baseUrl . "frontend/views/login.php");
--- frontend/views/dashboard.php
+++ frontend/views/dashboard.php
@@ -391,9 +391,6 @@ $userinfo = $stmt->fetchAll();
 
     <script src="../js/dashboard.js"></script>
 
-    <br>
-    <a href="../logout.php">Logout</a>
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
