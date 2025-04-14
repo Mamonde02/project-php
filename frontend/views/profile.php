ed3f24dab7e2fd820583e7e9165bc534967e7617
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-14 20:36:58 +0800

    call profile js

--- frontend/views/profile.php
+++ frontend/views/profile.php
@@ -178,14 +178,7 @@ if (!$userinfo) {
         </div>
     </div>
 
-    <script>
-        function openUpdateForm(id, username, email) {
-            // Set values in the input fields inside the modal
-            document.getElementById("id").value = id;
-            document.getElementById("username").value = username;
-            document.getElementById("email").value = email;
-        }
-    </script>
+    <script src="../js/profile.js"></script>
 
 
 
