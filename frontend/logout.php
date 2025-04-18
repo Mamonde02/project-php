5c3a23dcadd5c9cf2b37184c70e0f2fef01aaba2
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-18 13:51:35 +0800

    fix base url logout.php

--- frontend/logout.php
+++ frontend/logout.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
 session_destroy();
+require_once "../backend/config/baseconfig.php";
 // header("Location: frontend/views/login.php");
 // header("Location: views/login.php"); // Redirect to login page
 
-$baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/php-auth/';
-header("Location: " . $baseUrl . "frontend/views/login.php");
+// $baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/php-auth/';
+// $baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/';
+// header("Location: " . $baseUrl . "frontend/views/login.php");
+
+header("Location: " . BASE_URL . "frontend/views/login.php");
 
 exit();
 ?>
