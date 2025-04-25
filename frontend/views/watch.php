fc4f6f11606a86e35ba27db2e9987ecd502048b3
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-25 15:04:01 +0800

    add navbar on watch page

--- frontend/views/watch.php
+++ frontend/views/watch.php
@@ -55,6 +55,7 @@ $url = $data["data"][0]["url"];
 </head>
 
 <body class="bg-gray-100">
+    <?php include('components/navbar.php'); ?>
 
     <div class="container mx-auto px-4 py-10">
         <h1 class="text-3xl font-bold text-center mb-8">Anime List Collection</h1>
