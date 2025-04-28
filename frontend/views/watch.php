83ae43028f03a5d0be3372406b7128a071e41587
Mamonde02 <valerosojohnraymund@gmail.com>
2025-04-28 22:31:49 +0800

    call external watch

--- frontend/views/watch.php
+++ frontend/views/watch.php
@@ -110,36 +110,7 @@ $url = $data["data"][0]["url"];
         </div>
     </div>
 
-    <script>
-        const searchInput = document.getElementById('searchInput');
-        const typeFilter = document.getElementById('typeFilter');
-        const animeCards = document.querySelectorAll('.anime-card');
-
-        function applyFilters() {
-            const query = searchInput.value.toLowerCase();
-            const selectedType = typeFilter.value;
-
-            animeCards.forEach(card => {
-                const title = card.getAttribute('data-title');
-                const type = card.getAttribute('data-type');
-
-                const matchesTitle = title.includes(query);
-                const matchesType = selectedType === "" || type === selectedType;
-
-                card.style.display = matchesTitle && matchesType ? 'block' : 'none';
-            });
-        }
-
-        let debounceTimer;
-        searchInput.addEventListener('input', () => {
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(() => {
-                applyFilters();
-            }, 1000); // 1 second delay
-        });
-
-        typeFilter.addEventListener('change', applyFilters);
-    </script>
+    <script src="../js/watch.js"></script>
 
 </body>
 
