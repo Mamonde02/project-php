bdc9463d7ab64d427abf7338a7813e9146504537
Mamonde02 <valerosojohnraymund@gmail.com>
2025-05-09 17:37:15 +0800

    remove script tag index

--- index.php
+++ index.php
@@ -211,39 +211,11 @@
         © <?= date("Y") ?> HobbyTapp. All rights reserved.
     </footer>
 
-    <!-- <script>
-        function updateClock() {
-            const now = new Date();
-            const options = {
-                timeZone: 'Asia/Manila',
-                hour: 'numeric',
-                minute: 'numeric',
-                second: 'numeric',
-                hour12: true
-            };
-            const formatter = new Intl.DateTimeFormat([], options);
-            document.getElementById("liveClock").textContent = formatter.format(now);
-        }
-
-        setInterval(updateClock, 1000); // update every second
-        updateClock(); // run on page load
-
-
-        function toggleAccordion(id) {
-            const content = document.getElementById(`content-${id}`);
-            const icon = document.getElementById(`icon-${id}`);
-            content.classList.toggle('max-h-0');
-            content.classList.toggle('max-h-96');
-            icon.classList.toggle('rotate-180');
-        }
-    </script> -->
-
     <?php
-    // Include the JavaScript file
+    // Working instead of using the script tag
     echo '<script src="./frontend/js/index.js"></script>';
     ?>
 
-    <script scr="./frontend/js/index.js"></script>
 
 </body>
 
