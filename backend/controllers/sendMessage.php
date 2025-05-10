6576a5bfe0d96868a1c66056d6f6501cea05819f
Mamonde02 <valerosojohnraymund@gmail.com>
2025-05-10 22:37:53 +0800

    msg model applied send msg controller

--- backend/controllers/sendMessage.php
+++ backend/controllers/sendMessage.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 require_once "../../backend/config/database.php";
+require_once "../../backend/models/message.php";
+
+$messageModel = new Message($pdo);
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['user_id'])) {
     $sender_id = $_SESSION['user_id'];
@@ -10,8 +14,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['user_id'])) {
     error_log("sendMessage.php - Sender: $sender_id, Receiver: $receiver_id, Message: $message");
 
     if (!empty($message)) {
-        $stmt = $pdo->prepare("INSERT INTO messages (sender_id, receiver_id, message) VALUES (?, ?, ?)");
-        if ($stmt->execute([$sender_id, $receiver_id, $message])) {
+        // $stmt = $pdo->prepare("INSERT INTO messages (sender_id, receiver_id, message) VALUES (?, ?, ?)");
+        $sendMessage = $messageModel->sendMessage($sender_id, $receiver_id, $message);
+        if ($sendMessage) {
             error_log("Message successfully inserted into database.");
         } else {
             error_log("Failed to insert message.");
